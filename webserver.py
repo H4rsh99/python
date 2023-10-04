0654a3a9b9761dac2c876bb660c63f9cd297bfc3
H4rsh99 <91564266+H4rsh99@users.noreply.github.com>
2023-10-04 21:13:20 +0530

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+# Set the port you want the server to listen on
+PORT = 8080
+
+# Define the request handler
+Handler = http.server.SimpleHTTPRequestHandler
+
+# Create the server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    try:
+        # Start the server
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer stopped.")
